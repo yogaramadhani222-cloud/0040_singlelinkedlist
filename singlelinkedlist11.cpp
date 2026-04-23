669977b6b81336be8d3fbd6bc0461bfee1998232
yogaramadhani222-cloud <yogaramadhani222@gmail.com>
2026-04-23 16:37:44 +0700

    tambah fungsi dolNode

--- singlelinkedlist11.cpp
+++ singlelinkedlist11.cpp
@@ -55,4 +55,37 @@ public:
     bool listEmpty(){
         return (START == NULL);
     }
+
+    bool search(int nim, Node *&previous, Node *&current)
+    {
+        previous = START;
+        current = START;
+
+        while (current != NULL && nim != current->noMhs)
+        {
+            previous = current;
+            current = current->next;
+        }
+
+        return (current != NULL);
+    }
+
+    bool delNode(int nim)
+    {
+        Node *current, *previous;
+        if (!search(nim, previous, current))
+            return false;
+
+        if (current == START)
+        {
+            START = START->next;
+        }
+        else
+        {
+            previous->next = current->next;
+        }
+
+        delete current;
+        return true;
+    }
 };
